24a189650a2ef8e22c83362ab74f69b25d3d2e4c
bastionking <bastionking@hotmail.my>
2017-09-02 23:53:10 +0800

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+Product.delete_all
+#...
+Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
+                description:
+                %{<p>
+                  <em>Native Apps,Multiple Platform</em>
+                  Answer the question "Can we build this for all devices?" with
+                  a resounding YES. This book will help you get there with a real-world
+                  introduction to seven platforms, whether you’re new to mobile or an
+                  experienced developer needing to expand your options. Plus, you’ll find
+                  out which cross-platform solution makes the most sense for your needs.
+                  </p>},
+                image_url: "http://placehold.it/350x200",
+                price: 26.00)
+#...
